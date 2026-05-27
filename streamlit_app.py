2f191f6275e95e8048f3b3ff1fa7582031ceaaed
Nicholas Malloy <76117900+nmalloy13@users.noreply.github.com>
2026-05-27 13:15:05 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,5 +45,8 @@ if ingredients_list:
     #st.write(my_insert_stmt)
 
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+smoothiefroot_response = requests.get(
+    "https://my.smoothiefroot.com/api/fruit/watermelon"
+)
+
+st.json(smoothiefroot_response.json())
